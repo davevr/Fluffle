7ad9e43b1630699a61e8b0000da7ce4c38c9cd43
Dave Vronay <davevr@eweware.com>
2016-11-01 10:23:00 -0700

    android update

--- FluffyCore/Server.cs
+++ FluffyCore/Server.cs
@@ -31,7 +31,7 @@ namespace Fluffimax.Core
 		private static string localHostStr = "http://10.0.3.2:8080"; // "http://localhost:8080"; // "http://10.0.3.2:8080"
 		private static string networkHostStr = "http://192.168.0.22:8080";
 		private static string productionHostStr = "http://fluffle.it";
-		private static string serverBase = productionHostStr; 
+		private static string serverBase = localHostStr; 
 		private static string apiPath;
 		public static string SpriteImagePath;
 		public static string ProfileImagePath;
